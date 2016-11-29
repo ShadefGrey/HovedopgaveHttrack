3db7e9f098719dc99867c4af61256ca2885466d6
ckha <zpif@hotmail.com>
2016-11-29 13:37:34 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import pt.arquivo.httrack2arc.HTTrack2ArcConverter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * Created by ckha on 23-11-2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Date d = new Date();
+            String destination = "C:/Users/ckha/Desktop/httracktoarc/httracktest2";
+            if(!new File(destination).exists()){
+                File f = new File(destination);
+                f.mkdir();
+            }
+            HTTrack2ArcConverter htt = new HTTrack2ArcConverter(
+                    "C:\\Users\\ckha\\Desktop\\httracktoarc\\roende.dk", destination, d);
+            htt.run();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
